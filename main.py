a16b455d1b3d4aeff3fd15be6e42283c87b7dff8
max <maxlconway@gmail.com>
2025-01-27 17:06:03 -0700

    update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from run_expiriment import run_experiment
+
+"""
+Runs a series of experiments to compare the performance of different language models on the
+"""
 if __name__ == "__main__":
     run_experiment("gpt-3.5-turbo-0125",  "basic_prompt")
     run_experiment("gpt-4o-mini",  "basic_prompt")
