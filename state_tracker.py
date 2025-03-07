9f4c16f821c7a1ba4ac7978578fc7662a7ddac87
mateo <mateofeit@gmail.com>
2025-03-06 21:58:25 -0700

    Added more functions to ActionOperator should be done tn

--- state_tracker.py
+++ state_tracker.py
@@ -1,19 +1,22 @@
 import ast
 import regex as re
 from pyperplan.task import Operator
-from pyperplan.pddl.parser import Parser
+from pyperplan.pddl.parser import Parser, Predicate
 
 
+#TODO Parameterize '?y'
 class ActionOperator:
     """Taking better name suggestions please."""
+
     def __init__(self, state: tuple, action: str, domain_file: str, problem_file: str, formatted_action_file: str) -> None:
         self.state = state
         self.action = action
         self.domain_file = domain_file
         self.problem_file = problem_file
         self.formatted_action_file = formatted_action_file
+
     def get_action_information(self):
-        """Writes a file that takes transforms an action like
+        """Writes a file that transforms an action like
         "{""pick"": ""red_block"", ""place"": ""yellow_block""}"
         into a pyperplan compliant operator.
         """
@@ -21,7 +24,6 @@ class ActionOperator:
         formed_state = (re.sub(r'(\w+):\s*(\w+)', r"'\1': '\2'", formed_state))
         formed_state = ast.literal_eval(formed_state)
         assert type(formed_state) == dict, "Formed_state is not a dict"
-        print(f"Formed_state: {formed_state}, Type: {type(formed_state)}")
         unstackable = []
         pick_upable = []
         for key, value in formed_state.items():
@@ -53,12 +55,12 @@ class ActionOperator:
             if formed_action['pick'] in pick_upable:
                 f.write(f"(pick-up {formed_action['pick']})")
             else:
-                f.write(f"\n(unstack {formed_action['pick']})" + ')')
+                f.write(f"(unstack {formed_action['pick']})" + ')')
             if formed_action['place'] == 'table':
                 f.write(f"\n(put-down {formed_action['pick']}" + ')')
             else:
                 f.write(f"\n(stack {formed_action['pick']} {formed_action['place']}" + ')')
-        return None
+        return formed_action['pick'], formed_action['place']
     def get_domain_information(self) -> tuple[frozenset, frozenset, frozenset] or None:
         """Retrieves action preconditions, add effects, and del effects."""
         assert (type(self.domain_file) == str and type(self.problem_file) == str and
@@ -66,11 +68,12 @@ class ActionOperator:
         # Files must be strings
         with open(self.formatted_action_file, 'r') as f:
             first_line = f.readline().strip()
-            found_action = re.search(r"\(\s*(\S+)", first_line).group(1)
-            print(f"Action1: {found_action}")
+            found_action = re.search(r"\(\s*(\S+)", first_line)
             # Extracting action from format_action
             if not found_action:
                 raise ValueError("Action isnt valid.")
+            else:
+                found_action = found_action.group(1)
             # Action must be valid yk
         parser = Parser(self.domain_file, self.problem_file)
         domain_parsed = parser.parse_domain(read_from_file=True)
@@ -89,26 +92,61 @@ class ActionOperator:
         parser = Parser(self.domain_file, self.problem_file)
         domain_parsed = parser.parse_domain(read_from_file=True)
         problem_parsed = parser.parse_problem(domain_parsed, read_from_file=True)
-        print(f"Problem: {problem_parsed} typd: {type(problem_parsed)}")
-        preconditions = problem_parsed.goal
-        #Which dumbass refers to preconditions as goal wtffff
-        print(f"preconditions: {preconditions}")
-        return frozenset(preconditions)
-    def get_missing_preconditions(self) -> None:
-        preconditions, _, _  = self.get_domain_information()
-        print(f"preconditions: {preconditions}")
-        missing_preconditions = preconditions - self.get_state_preconditions()
-        print(f"missing_preconditions: {missing_preconditions}")
+        preconditions = frozenset(problem_parsed.initial_state)
+        return preconditions
+    def get_operator_bindings(self, b1, b2) -> dict:
+        """Action predicates are naturally parameterized.
+        This function retrieves the parameters based on some pddl logic."""
+        #Blocks are clear if there is nothing above them, which means that the block
+        #being picked up needs to be clear, which is our ?x variable. Our y variable
+        #is just our other block
+        op_bindings = {}
+        x_var, y_var = self.get_action_information()
+        op_bindings[f'{b1}'] = x_var
+        op_bindings[f'{b2}'] = y_var
+        #Variables we found earlier.
+        op_bindings['[object]'] = 'object'
+        print(f"Operator bindings: {op_bindings}")
+        return op_bindings
+
+    @staticmethod
+    def sort_preconds(action_precond: Predicate, state_precond: Predicate):
+        action_predicate_list = list(action_precond)
+        action_predicate_list_sorted = sorted(action_predicate_list, key=lambda x: x.name)
+        state_predicate_list = list(state_precond)
+        state_predicate_list_sorted = sorted(state_predicate_list, key=lambda x: x.name)
+        print(f"action_predicate_list_sorted: {action_predicate_list_sorted}")
+        print(f"state_predicate_list_sorted: {state_predicate_list_sorted}")
+        return action_predicate_list_sorted, state_predicate_list_sorted
+
+    def plug_parameters(self, action_predcon: list, bdict: dict, pA, pB, pC):
+        """Meant to be called on a single string."""
+        #Just add more variables for future, more complicated worlds.
+        deparamed_action_preda = re.sub(pA, bdict[pA], action_predcon)
+        deparamed_action_predb = re.sub(pB, bdict[pB], deparamed_action_preda)
+        deparamed_action_predc = re.sub(pC, bdict[pC], deparamed_action_predb)
+        print(f"deparamed_action_pred: {deparamed_action_predc}")
+        return deparamed_action_predc
+    def get_missing_preconds(self, action_preconditions, state_preconditions):
+        numb_precons = len(action_preconditions)
+        cleared_precons = set()
+        missing = set()
+        #Using a set for no duplicate values.
+        for index, precon in enumerate(action_preconditions):
+            action_preconditions[index] = self.plug_parameters(precon, self.get_operator_bindings('?x', '?y'), r"|?x", r"\?y", r"\object")
+        #Fix action preconditions
+        for action_precondition in action_preconditions:
+            if action_precondition not in state_preconditions:
+                missing.add(action_precondition)
+            else:
+                cleared_precons.add(action_precondition)
+        return missing, cleared_precons
 
     def get_state_prime(self) -> None:
         return None
     def apply_operator(self, operator: Operator):
         extracted_state = (self.state[1])
-        # Problem is that state is not in the same format as operator
-        # Problem is that state needs preconditions
-        print(f"Preconditions: {operator.preconditions}")
         missing_conditions = operator.preconditions - extracted_state
-        print(f"Missing conditions: {missing_conditions}")
         op_applicable = operator.applicable(extracted_state)
         # Checks if the operator is applicable
         print(f"Operator applicable: {op_applicable}")
@@ -122,9 +160,12 @@ class ActionOperator:
 
 if __name__=='__main__':
     example_action = "{""pick"": ""red_block"", ""place"": ""yellow_block""}"
-    example_state = "{""red_block"": ""table"", ""blue_block"": ""table"", ""yellow_block"": ""table""}","{""red_block"": ""table"", ""blue_block"": ""table"", ""yellow_block"": ""table""}","{""pick"": ""None"", ""place"": ""None""}","""0"""
+    example_state = "{""red_block"": ""blue_block"", ""blue_block"": ""table"", ""yellow_block"": ""table""}","{""red_block"": ""yellow_block"", ""blue_block"": ""table"", ""yellow_block"": ""table""}","{""pick"": ""None"", ""place"": ""None""}","""0"""
     ao = ActionOperator(state=example_state, action=example_action, domain_file='domain.pddl',
                         problem_file='problem.pddl', formatted_action_file='hehe.pddl.soln')
-    precondiditons = ao.get_state_preconditions()
-    print(f"precondiditons: {precondiditons}")
-    ao.get_missing_preconditions()
+    state = ao.get_state_preconditions()
+    predicates, _, _ = ao.get_domain_information()
+    op_bindings = ao.get_operator_bindings('?x', "?y")
+
+
+
